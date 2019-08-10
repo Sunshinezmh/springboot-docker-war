948b19906c45a13bc26a059737686029aed4b15c
sunshine <18333640045@163.com>
2019-08-10 11:32:02 +0800

    提交修改的pom文件-张明慧-2019年8月10日11:31:57

--- src/main/java/com/dmsmbj/docker/springbootdockerwar/SpringbootDockerWarApplication.java
+++ src/main/java/com/dmsmbj/docker/springbootdockerwar/SpringbootDockerWarApplication.java
@@ -2,9 +2,20 @@ package com.dmsmbj.docker.springbootdockerwar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-public class SpringbootDockerWarApplication {
+@RestController
+public class SpringbootDockerWarApplication extends SpringBootServletInitializer{
+
+	@RequestMapping("/")
+	public String home() {
+		return "docker app running";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerWarApplication.class, args);
