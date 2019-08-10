2cbadfeca5fca0a2c53eba77d0cba866568c15d0
sunshine <18333640045@163.com>
2019-08-10 10:43:00 +0800

    first

---
+++ src/main/java/com/dmsmbj/docker/springbootdockerwar/SpringbootDockerWarApplication.java
@@ -0,0 +1,13 @@
+package com.dmsmbj.docker.springbootdockerwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerWarApplication.class, args);
+	}
+
+}
